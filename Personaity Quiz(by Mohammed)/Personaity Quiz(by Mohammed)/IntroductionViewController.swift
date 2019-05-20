e596a7ecba12e7d66336f9b8a3cc696f211b5480
SummairB <48170270+Summair@users.noreply.github.com>
2019-05-20 12:56:28 -0500

    Add files via upload
    Simple Personality Quiz on Swift/Xcode

---
+++ Personaity Quiz(by Mohammed)/Personaity Quiz(by Mohammed)/IntroductionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Personaity Quiz(by Mohammed)
+//
+//  Created by Mohammed Brohi on 3/21/19.
+//  Copyright © 2019 Mohammed Brohi. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    @IBAction func unwindtoQuizIntroduction(segue: UIStoryboard){
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
